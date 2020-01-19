c20b40ac78e3c407a5a55633b333c3c3b245d287
Marek Klatt <531devv@gmail.com>
2020-01-19 18:00:46 +0100

    Fix animation and text position on mobile screen

--- js/main.js
+++ js/main.js
@@ -8,13 +8,19 @@
     const background = document.querySelector('.slider');
 
     const contentChanger = (color, image) => {
-        TweenMax.from(imgContent, 5, { y: 1000});
+        TweenLite.set(imgContent, {clearProps:"all"});
+        TweenLite.set(background, {clearProps:"all"});
+        TweenLite.set(img, {clearProps:"all"});
+        TweenLite.set(contentNav, {clearProps:"all"});
+
+        TweenMax.from(imgContent, 5, { y: 1000 });
         TweenMax.from(background, 5, { width: 0 })
         TweenMax.to(background, 1, { backgroundColor: color,  ease:Power4.easeInOut });
         TweenMax.to(img, 0.1, {attr:{src: image}});
+        TweenMax.from(contentNav, 5, { opacity: 0, x: 500 })
     }
 
-    TweenMax.from(document.body, 5, {opacity: 0})
+    TweenMax.from(document.body, 5, {opacity: 0 })
     TweenMax.from(contentNav, 5, { opacity: 0, x: 300});
     TweenMax.from(img, 5, { y: 500 })
     TweenMax.from(socialIcons, 5, { y: 300, opacity: 0 });
@@ -32,21 +38,21 @@
     });
 
     document.body.addEventListener('click', (e) => {
-        if(e.target.matches('.content-nav-link')) {
-            switch(e.target.id){
-                case '1':
-                    contentChanger('#FE997C', 'images/one.png');
-                    break;
-                case '2':
-                    contentChanger('#352818', 'images/smoking-women.jpg');
-                    break;
-                case '3':
-                    contentChanger('#12a49d', 'images/door.jpg');
-                    break;
-                case '4':
-                    contentChanger('#6D8245', 'images/male.jpg');
-                    break;
-            }
+                if(e.target.matches('.content-nav-link')) {
+                switch(e.target.id){
+                    case '1':
+                        contentChanger('#FE997C', 'images/one.png');
+                        break;
+                    case '2':
+                        contentChanger('#352818', 'images/smoking-women.jpg');
+                        break;
+                    case '3':
+                        contentChanger('#12a49d', 'images/door.jpg');
+                        break;
+                    case '4':
+                        contentChanger('#6D8245', 'images/male.jpg');
+                        break;
+                }
         }
     });
 })();
